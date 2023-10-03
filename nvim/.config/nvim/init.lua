3fef3499199d2dfc9b027e4837c7c92416d0973f
Kevin Gyori <gyorijonatan@gmail.com>
2023-10-03 14:35:18 +0100

    nvim refact

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -82,7 +82,7 @@ else
     },
 
     -- Useful plugin to show you pending keybinds.
-    { 'folke/which-key.nvim',  opts = {} },
+    { 'folke/which-key.nvim',   opts = {} },
     {
       -- Adds git related signs to the gutter, as well as utilities for managing changes
       'lewis6991/gitsigns.nvim',
@@ -116,43 +116,6 @@ else
     },
 
     {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      build = ":Copilot auth",
-      opts = {
-        auto_trigger = true,
-        suggestion = { enabled = true },
-        panel = { enabled = true },
-        filetypes = {
-          markdown = true,
-          help = true,
-        },
-      },
-    },
-
-    -- Github theme
-    {
-      'projekt0n/github-nvim-theme',
-      lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-      priority = 1000, -- make sure to load this before all the other start plugins
-    },
-    -- Auto switch themes based on system setting
-    {
-      "f-person/auto-dark-mode.nvim",
-      config = {
-        update_interval = 5000,
-        set_dark_mode = function()
-          vim.api.nvim_set_option("background", "dark")
-          vim.cmd("colorscheme github_dark_colorblind")
-        end,
-        set_light_mode = function()
-          vim.api.nvim_set_option("background", "light")
-          vim.cmd("colorscheme github_light")
-        end,
-      },
-    },
-
-    {
       -- Set lualine as statusline
       'nvim-lualine/lualine.nvim',
       -- See `:help lualine.txt`
@@ -170,47 +133,22 @@ else
       -- Add indentation guides even on blank lines
       'lukas-reineke/indent-blankline.nvim',
       -- Enable `lukas-reineke/indent-blankline.nvim`
-      -- See `:help indent_blankline.txt`
+      -- See `:help indent-blankline.txt`
       config = function()
         require('ibl').setup {
-          char = '┊',
+          indent = { char = "┊" },
           show_trailing_blankline_indent = false,
         }
       end,
     },
 
     -- "gc" to comment visual regions/lines
-    { 'numToStr/Comment.nvim', opts = {} },
-
-    -- Fuzzy Finder (files, lsp, etc)
     {
-      'nvim-telescope/telescope.nvim',
-      branch = '0.1.x',
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-        -- Only load if `make` is available. Make sure you have the system
-        -- requirements installed.
-        {
-          'nvim-telescope/telescope-fzf-native.nvim',
-          -- NOTE: If you are having trouble with this installation,
-          --       refer to the README for telescope-fzf-native for more instructions.
-          build = 'make',
-          cond = function()
-            return vim.fn.executable 'make' == 1
-          end,
-        },
-      },
+      'numToStr/Comment.nvim',
+      opts = {}
     },
 
-    {
-      -- Highlight, edit, and navigate code
-      'nvim-treesitter/nvim-treesitter',
-      dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-      },
-      build = ':TSUpdate',
-    },
+
 
     -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
     --       These are some example plugins that I've included in the kickstart repository.
@@ -218,18 +156,32 @@ else
     require 'kickstart.plugins.autoformat',
     -- require 'kickstart.plugins.debug',
 
-    -- my custom plugins
-    -- File explorer
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-      }
-    },
-    "mbbill/undotree",
+    ----------- my custom plugins
+
+    -- Github theme
+    -- {
+    --   'projekt0n/github-nvim-theme',
+    --   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    --   priority = 1000, -- make sure to load this before all the other start plugins
+    -- },
+
+
+    -- Auto switch themes based on system setting
+    -- {
+    --   "f-person/auto-dark-mode.nvim",
+    --   config = {
+    --     update_interval = 5000,
+    --     set_dark_mode = function()
+    --       vim.api.nvim_set_option("background", "dark")
+    --       vim.cmd("colorscheme kanagawa-dragon")
+    --     end,
+    --     set_light_mode = function()
+    --       vim.api.nvim_set_option("background", "light")
+    --       vim.cmd("colorscheme kanagawa-lotus")
+    --     end,
+    --   },
+    -- },
+
 
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -237,186 +189,21 @@ else
     --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
     --
     --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-    -- { import = 'custom.plugins' },
+    { import = 'custom.plugins' },
   }, {})
 
-  -- [[ Setting options ]]
+  ----------------------------------------- [[ Configure nvim-cmp ]] -----------------------------------------
   -- See `:help vim.o`
   -- NOTE: You can change these options as you wish!
+  -- import 'custom.options'
+  require('custom.options')
 
-  -- Set highlight on search
-  vim.o.hlsearch = false
 
-  -- Make line numbers default
-  vim.wo.number = true
+  ----------------------------------------- [[ Basic Keymaps ]] -----------------------------------------
+  require('custom.remap')
 
-  -- Enable mouse mode
-  vim.o.mouse = 'a'
 
-  -- Sync clipboard between OS and Neovim.
-  --  Remove this option if you want your OS clipboard to remain independent.
-  --  See `:help 'clipboard'`
-  vim.o.clipboard = 'unnamedplus'
-
-  -- Enable break indent
-  vim.o.breakindent = true
-
-  -- Save undo history
-  vim.o.undofile = true
-
-  -- Case-insensitive searching UNLESS \C or capital in search
-  vim.o.ignorecase = true
-  vim.o.smartcase = true
-
-  -- Keep signcolumn on by default
-  vim.wo.signcolumn = 'yes'
-
-  -- Decrease update time
-  vim.o.updatetime = 250
-  vim.o.timeoutlen = 300
-
-  -- Set completeopt to have a better completion experience
-  vim.o.completeopt = 'menuone,noselect'
-
-  -- NOTE: You should make sure your terminal supports this
-  vim.o.termguicolors = true
-
-  -- [[ Basic Keymaps ]]
-
-  -- File explorer nvim-tree
-  vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>')
-  -- nnoremap <leader>s :Neotree float git_status<cr>
-
-  -- undotree
-  vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<cr>')
-
-  -- Keymaps for better default experience
-  -- See `:help vim.keymap.set()`
-  vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
-  -- Remap for dealing with word wrap
-  vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-  vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
-  -- [[ Highlight on yank ]]
-  -- See `:help vim.highlight.on_yank()`
-  local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-  vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
-  })
-
-  -- [[ Configure Telescope ]]
-  -- See `:help telescope` and `:help telescope.setup()`
-  require('telescope').setup {
-    defaults = {
-      mappings = {
-        i = {
-          ['<C-u>'] = false,
-          ['<C-d>'] = false,
-        },
-      },
-    },
-  }
-
-  -- Enable telescope fzf native, if installed
-  pcall(require('telescope').load_extension, 'fzf')
-
-  -- See `:help telescope.builtin`
-  vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-  vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-  vim.keymap.set('n', '<leader>/', function()
-    -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-      winblend = 10,
-      previewer = false,
-    })
-  end, { desc = '[/] Fuzzily search in current buffer' })
-
-  vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-  vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-  vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-  vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-  vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-  vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-
-  -- [[ Configure Treesitter ]]
-  -- See `:help nvim-treesitter`
-  require('nvim-treesitter.configs').setup {
-    -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
-
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
-
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
-    },
-  }
-
-  -- Diagnostic keymaps
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-  -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
-  -- [[ Configure LSP ]]
+  ----------------------------------------- [[ Configure LSP ]] -----------------------------------------
   --  This function gets run when an LSP connects to a particular buffer.
   local on_attach = function(_, bufnr)
     -- NOTE: Remember that lua is a real programming language, and as such it is possible
@@ -459,7 +246,7 @@ else
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
       vim.lsp.buf.format()
     end, { desc = 'Format current buffer with LSP' })
-    nmap('<leader>ff', vim.lsp.buf.format, '[F]ormat [F]ile')
+    nmap('<leader>fo', vim.lsp.buf.format, '[F][O]rmat file')
   end
 
   -- Enable the following language servers
@@ -511,7 +298,7 @@ else
     end
   }
 
-  -- [[ Configure nvim-cmp ]]
+  ----------------------------------------- [[ Configure nvim-cmp ]] -----------------------------------------
   -- See `:help cmp`
   local cmp = require 'cmp'
   local luasnip = require 'luasnip'
@@ -534,24 +321,24 @@ else
         behavior = cmp.ConfirmBehavior.Replace,
         select = true,
       },
-      ['<Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.locally_jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
+      -- ['<Tab>'] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     cmp.select_next_item()
+      --   elseif luasnip.expand_or_locally_jumpable() then
+      --     luasnip.expand_or_jump()
+      --   else
+      --     fallback()
+      --   end
+      -- end, { 'i', 's' }),
+      -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     cmp.select_prev_item()
+      --   elseif luasnip.locally_jumpable(-1) then
+      --     luasnip.jump(-1)
+      --   else
+      --     fallback()
+      --   end
+      -- end, { 'i', 's' }),
     },
     sources = {
       { name = 'nvim_lsp' },
---
+++ nvim/.config/nvim/lua/custom/plugins/neotree.lua
@@ -0,0 +1,23 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup(
+			{
+				filesystem = {
+					hide_dotfiles = true,
+				},
+				window = {
+					mappings = {
+						["l"] = "open",
+					},
+				},
+			}
+		)
+	end
+}
