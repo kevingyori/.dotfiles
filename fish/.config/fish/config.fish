6fea63fb3f524438ac8b0735906ba5905ea33a9f
Kevin Gyori <gyorijonatan@gmail.com>
2023-11-27 18:37:52 +0000

    apply the correct cursor in fish vim mode

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -16,6 +16,25 @@ export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 # bun end
 
+# Fix cursor shape in the shell
+# Only run this in interactive shells
+if status is-interactive
+
+  # I'm trying to grow a neckbeard
+  # fish_vi_key_bindings
+  # Set the cursor shapes for the different vi modes.
+  set fish_cursor_default     block      blink
+  set fish_cursor_insert      line       blink
+  set fish_cursor_replace_one underscore blink
+  set fish_cursor_visual      block
+
+  function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
+  end
+end
+
 starship init fish | source
 
 # tabtab source for packages
