21d0f57bde66e9d7a3d29f6e0ea18ca8ccfb55fd
Kevin Gyori <gyorijonatan@gmail.com>
2024-04-11 11:24:18 +0100

    change lazyvim theme

--- lazy/.config/LazyVim/lua/plugins/colorscheme.lua
+++ lazy/.config/LazyVim/lua/plugins/colorscheme.lua
@@ -1,35 +1,40 @@
 return {
-  -- { "sainnhe/gruvbox-material" },
-  {
-    "tinted-theming/base16-vim",
-    lazy = false,
-    priority = 5000,
-    config = function()
-      local cmd = vim.cmd
-      local g = vim.g
-      local current_theme_name = os.getenv("BASE16_THEME")
-      if current_theme_name and g.colors_name ~= "base16-" .. current_theme_name then
-        cmd("let base16colorspace=256")
-        cmd("colorscheme base16-" .. current_theme_name)
-      end
-    end,
-  },
-  {
-    "RRethy/nvim-base16",
-    lazy = false,
-    priority = 50000,
-    enabled = true,
-    config = function()
-      require("base16-colorscheme").with_config({
-        telescope = false,
-        indentblankline = true,
-        notify = true,
-        ts_rainbow = true,
-        cmp = true,
-        illuminate = true,
-      })
-    end,
-  },
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   lazy = false,
+  --   priority = 5000,
+  -- },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- {
+  --   "tinted-theming/base16-vim",
+  --   lazy = false,
+  --   priority = 5000,
+  --   config = function()
+  --     local cmd = vim.cmd
+  --     local g = vim.g
+  --     local current_theme_name = os.getenv("BASE16_THEME")
+  --     if current_theme_name and g.colors_name ~= "base16-" .. current_theme_name then
+  --       cmd("let base16colorspace=256")
+  --       cmd("colorscheme base16-" .. current_theme_name)
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "RRethy/nvim-base16",
+  --   lazy = false,
+  --   priority = 50000,
+  --   enabled = true,
+  --   config = function()
+  --     require("base16-colorscheme").with_config({
+  --       telescope = false,
+  --       indentblankline = true,
+  --       notify = true,
+  --       ts_rainbow = true,
+  --       cmp = true,
+  --       illuminate = true,
+  --     })
+  --   end,
+  -- },
   -- { "kvrohit/mellow.nvim" },
   -- { "nyoom-engineering/oxocarbon.nvim" },
   -- {
@@ -51,7 +56,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "base16-black-metal-bathory",
+      colorscheme = "catppuccin",
     },
   },
 }
