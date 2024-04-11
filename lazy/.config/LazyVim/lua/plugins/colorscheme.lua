098a5d6d504698a35c741096ea4b07c72b444103
kevingyori <gyorijonatan@gmail.com>
2024-04-11 06:50:10 +0100

    lazyvim theme change

--- lazy/.config/LazyVim/lua/plugins/colorscheme.lua
+++ lazy/.config/LazyVim/lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
-  { "sainnhe/gruvbox-material" },
+  -- { "sainnhe/gruvbox-material" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "catppuccin",
     },
   },
 }
