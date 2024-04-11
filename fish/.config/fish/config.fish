d1a22bf327c474ed33e072f45c326f43f45742c4
Kevin Gyori <gyorijonatan@gmail.com>
2024-04-11 11:23:32 +0100

    add new fish PATH to go

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -48,3 +48,6 @@ end
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+# Go
+set -x PATH /Users/kevingyori/go/bin "$PATH"
